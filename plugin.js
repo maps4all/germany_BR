98647b2d324938c203956da2f47db739111367b8
maps4all <maps4all007@yandex.com>
2017-08-08 22:42:44 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,6 @@
+{
+    "moduleId" : "grabowCommuter/germany_BR",    
+    "type" : "RSS",    
+    "name" : "Bayrischer Rundfunk",    
+    "uri" : "http://www.br.de/verkehr-static/verkehrsmeldungen-rss.xml"
+}
